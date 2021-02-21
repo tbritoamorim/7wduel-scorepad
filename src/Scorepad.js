27aa79cdfc57a1c6973add87eab99a033dc61b9c
Thiago Amorim <thiago.amorim@seatecnologia.com.br>
2021-02-21 03:02:39 -0300

    changing Object.assign for spread operator

--- src/Scorepad.js
+++ src/Scorepad.js
@@ -1,31 +1,28 @@
 import React, { useState } from 'react';
-/*import { ReactComponent as BlueCard} from "./icons/blue-cards.svg";
-import greenCard from "./icons/green-cards.svg";
-import yellowCard from "./icons/yellow-cards.svg";
-import purpleCard from "./icons/purple-cards.svg";*/
-import './scorepad.scss';
 import TableRow from './TableRow';
+import './scorepad.scss';
 
 
 export default function Scorepad() {
-  const [pointsP1, setPointsP1] = useState({
-    'blue': 0,
+  const [points, setPoints] = useState({
+    'P1': {'blue': 0},
+    'P2': {'blue': 0}
   });
-  const [totalP1, setTotalP1] = useState(0);
-  const [pointsP2, setPointsP2] = useState({
-    'blue': 0,
+  const [total, setTotal] = useState({
+    'P1': 0,
+    'P2': 0
   });
-  const [totalP2, setTotalP2] = useState(0);
-
 
   const handleSetPoints = (color, newPoints, player) => {
-    if(player === "P1") {
-      setPointsP1(Object.assign(pointsP1, {[`${color}`]: newPoints}));
-      setTotalP1(Object.values(pointsP1).reduce((a, b) =>  a + b ));
-    } else {
-      setPointsP2(Object.assign(pointsP2, {[`${color}`]: newPoints}));
-      setTotalP2(Object.values(pointsP2).reduce((a, b) =>  a + b ));
-    }
+      let playerPoints = {[`${player}`]: {...points[`${player}`], [`${color}`]: newPoints}};
+      let playerTotalInt = Object.values(playerPoints[`${player}`]).reduce((a, b) =>  a + b );
+      let playerTotal = {[`${player}`]: playerTotalInt};
+      setPoints(prevState => {
+        return {...prevState, ...playerPoints}
+      });
+      setTotal(prevState => {
+        return {...prevState, ...playerTotal}
+      });
   }
 
     return (
@@ -49,8 +46,8 @@ export default function Scorepad() {
             <TableRow color="military" icon="Military" sumF={handleSetPoints}/>
             <tr>
               <td className="sigma">Σ</td>
-              <td><span id="result1" className="sigma">{totalP1}</span></td>
-              <td><span id="result2" className="sigma">{totalP2}</span></td>
+              <td><span id="result1" className="sigma">{total.P1}</span></td>
+              <td><span id="result2" className="sigma">{total.P2}</span></td>
             </tr>
           </tbody>
         </table>
